c0dc21f2b5187a8e1c2e46950f500b6c5f750f2e
Camille Rheaume-Tremblay <camillerheaumet@hotmail.com>
2018-08-14 16:56:43 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name)
+  messages = []
+  name.each do |i|
+    s = badge_maker(i)
+    messages.push(s)
+  end
+  messages
+end
+
+def assign_rooms(name)
+  rooms = []
+  message = []
+  name.each_with_index {|name, index|
+    rooms = index
+    message.push("Hello, #{name}! You'll be assigned to room #{index +1}!")
+  }
+  message
+end
+
+#def printer(rooms)
+#  assign_rooms(name)
+#  rooms.each |i|
+#    puts i
+#  end
+#end
